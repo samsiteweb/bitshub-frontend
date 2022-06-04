653a59006c93b88ffff2eecd876a287af6f051fb
Ifedayo Agboola <muller@Ifedayos-MacBook-Pro.local>
2022-06-04 23:03:42 +0100

    categories dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,41 @@
 import React from "react";
+import game from "../images/game.svg";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800">
       <div className="container flex">
-        <div className="px-8 py-4 bg-primary flex items-center cursor-pointer">
+        <div className="px-8 py-4 bg-primary flex items-center cursor-pointer relative group">
           <span className="text-white">
             <i className="fas fa-bars"></i>
           </span>
           <span className="capitalize ml-2 text-white">All categories</span>
+          <div className="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
+            <a href="" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+              <img src={game} alt="" className="w-5 h-5 object-contain" />
+              <span className="ml-6 text-gray-600 text-sm">Gaming</span>
+            </a>
+            <a href="" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+              <img src="" alt="" className="w-5 h-5 object-contain" />
+              <span className="ml-6 text-gray-600 text-sm">Office</span>
+            </a>
+            <a href="" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+              <img src="" alt="" className="w-5 h-5 object-contain" />
+              <span className="ml-6 text-gray-600 text-sm">Personal</span>
+            </a>
+            <a href="" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+              <img src="" alt="" className="w-5 h-5 object-contain" />
+              <span className="ml-6 text-gray-600 text-sm">Desktops</span>
+            </a>
+            <a href="" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+              <img src="" alt="" className="w-5 h-5 object-contain" />
+              <span className="ml-6 text-gray-600 text-sm">Sleek</span>
+            </a>
+            <a href="" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+              <img src="" alt="" className="w-5 h-5 object-contain" />
+              <span className="ml-6 text-gray-600 text-sm">components</span>
+            </a>
+          </div>
         </div>
         <div className="flex items-center justify-between flex-grow pl-12">
           <div className="flex items-center space-x-6 capitalize">
