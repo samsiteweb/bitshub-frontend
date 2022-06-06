cb09feadf110c8996dbf027621f857873ef73ea3
Ifedayo Agboola <muller@Ifedayos-MacBook-Pro.local>
2022-06-06 23:23:35 +0100

    added features cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import Header from "./components/Header";
 import Banner from "./pages/Banner";
+import game from "./images/game.svg";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <div className="container py-16">
+        <div className="w-10/12 grid grid-cols-3 gap-6 mx-auto justify-center">
+          <div className="border border-primary rounded-sm px-3 py-6 justify-center flex items-center gap-5">
+            <img src={game} alt="" className="w-12 h-12 object-contain" />
+            <div>
+              <h4 className="font-medium capitalize text-lg">Free shoping</h4>
+              <p className="text-gray-500 text-sm">order over N200,000</p>
+            </div>
+          </div>
+          <div className="border border-primary rounded-sm px-3 py-6 justify-center flex items-center gap-5">
+            <img src={game} alt="" className="w-12 h-12 object-contain" />
+            <div>
+              <h4 className="font-medium capitalize text-lg">Money Returns</h4>
+              <p className="text-gray-500 text-sm">15 days money return</p>
+            </div>
+          </div>
+          <div className="border border-primary rounded-sm px-3 py-6 justify-center flex items-center gap-5">
+            <img src={game} alt="" className="w-12 h-12 object-contain" />
+            <div>
+              <h4 className="font-medium capitalize text-lg">24/7 Support</h4>
+              <p className="text-gray-500 text-sm">Customer support</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
