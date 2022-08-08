eeb18dc784c49f730f21c12c7318ca0c2e456e43
Ifedayo Agboola <muller@Ifedayos-MacBook-Pro.local>
2022-08-08 20:39:50 +0100

    created backend

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,20 @@
-import React from "react";
-import { ProductData } from "../data/productData";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "./Product";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="grid lg:grid-cols-4 gap-3 md:grid-cols-3 sm:grid-cols-2">
-      {ProductData.products.map((item) => {
+      {products.map((item) => {
         return <Product item={item} id={item.id} />;
       })}
     </div>
