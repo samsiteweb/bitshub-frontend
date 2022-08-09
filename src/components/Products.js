fa5bc929c068f1b1669fdbd45931a5bead913bb6
Ifedayo Agboola <muller@Ifedayos-MacBook-Pro.local>
2022-08-09 05:36:36 +0100

    built loaders and modals

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,22 +1,42 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Product from "./Product";
+import MessageBox from "./modal/MessageBox";
+import LoadingBox from "./LoadingBox";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
+
   return (
-    <div className="grid lg:grid-cols-4 gap-3 md:grid-cols-3 sm:grid-cols-2">
-      {products.map((item) => {
-        return <Product item={item} id={item.id} />;
-      })}
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="grid lg:grid-cols-4 gap-3 md:grid-cols-3 sm:grid-cols-2">
+          {products.map((item) => {
+            return <Product item={item} id={item.id} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
