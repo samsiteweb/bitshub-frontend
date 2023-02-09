c11d2f1940d3d0d6e88852d4aed2aa0bed7d7f20
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-09 17:40:12 +0100

    started

--- src/screens/AccountLayout.js
+++ src/screens/AccountLayout.js
@@ -8,7 +8,10 @@ const AccountLayout = ({ children }) => {
       <Breadcrumbs page="Account" />
       <div className="container grid grid-cols-12 items-start gap-6 pt-4 pb-16 overflow-x-scroll h-[70vh]">
         <ProfileSideNav />
-        {children}
+        <div className=" h-[60vh] w-[37.5vw] overflow-y-scroll">
+          {children}
+        </div>
+        
         
       </div>
     </div>
