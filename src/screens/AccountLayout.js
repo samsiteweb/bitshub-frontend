8c6864fe3d590b3a18bdd3ef86fcfa767c499cf5
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-11 17:33:17 +0100

    fixed sidebar and shop mobile view

--- src/screens/AccountLayout.js
+++ src/screens/AccountLayout.js
@@ -6,7 +6,7 @@ const AccountLayout = ({ children }) => {
   return (
     <div>
       <Breadcrumbs page="Account" />
-      <div className="container grid grid-cols-12 items-start gap-6 pt-4 pb-16 overflow-x-scroll h-[70vh]">
+      <div className="container grid grid-cols-12 items-start gap-6 pt-4 pb-16">
         <ProfileSideNav />
         {children}
         
